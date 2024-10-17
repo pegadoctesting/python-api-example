427f5eb97d27b0ebd062807e6a5913deeebb52a9
pegadoctesting <pegadoctesting123@gmail.com>
2024-10-17 12:55:49 -0700

    Changes

--- main_template.py
+++ main_template.py
@@ -2,24 +2,28 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flasgger import Swagger
 
+import yfinance as yf
+import pandas as pd
+import pandas_ta as ta
+import time
+
 app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
-
-class UppercaseText(Resource):
-
+    
+class TickerInfo(Resource):
     def get(self):
         """
-        This method responds to the GET request for this endpoint and returns the data in uppercase.
+        Call Yfinance to retrieve data
         ---
         tags:
-        - Text Processing
+        - YFinance
         parameters:
-            - name: text
+            - name: Tickers
               in: query
               type: string
               required: true
-              description: The text to be converted to uppercase
+              description: List of Tickers seperated by commas
         responses:
             200:
                 description: A successful GET request
@@ -29,14 +33,65 @@ class UppercaseText(Resource):
                         type: object
                         properties:
                             text:
-                                type: string
-                                description: The text in uppercase
+                                type: objects
+                                description: list of ticker info
         """
-        text = request.args.get('text')
+        tickers = str(request.args.get('Tickers'))
 
-        return jsonify({"text": text.upper()})
+        ticker_list = tickers.split(",")
+        print(ticker_list)
+        data = {'Tickers': []}
+        for item in ticker_list:
+            item_data = self.get_indicators(item)
+            if (type(item_data) != int ):
+                data['Tickers'].append(item_data)
+            else:
+                data['Tickers'].append({"Ticker": "",
+                "MACD": 0.0,
+                "MACD_Signal": 0.0,
+                "RSI": 0.0,
+                "STOCH_K": 0.0,
+                "B_Low": 0.0,
+                "B_Mid": 0.0,
+                "B_Upper": 0.0,
+                "SMA50": 0.0,
+                "SMA200": 0.0,
+                "52WeekHigh": 0.0,
+                "Open": 0.0,
+                "time_elapsed": 0.0})
 
-api.add_resource(UppercaseText, "/uppercase")
+        return jsonify(data)
+
+    def get_indicators(self,ticker):
+        start = time.time()
+        try:
+            symbol = yf.Ticker(ticker)
+            df = symbol.history(interval='1d', period='1y')
+            newdf = pd.DataFrame()
+            newdf.insert(0,"Ticker", "")
+            newdf.ta.macd(close=df["Close"], fast=12, slow=26, signal=9, append=True, col_names=("MACD","MACD_Histogram","MACD_Signal"))
+            newdf.ta.rsi(close=df["Close"], length=20, append=True, col_names=("RSI",))
+            newdf.ta.stoch(high=df["High"], low=df["Low"], close=df["Close"], append=True, col_names=("STOCH_K", "STOCH_D"))
+            newdf.ta.bbands(close=df["Close"], length=20, append=True, col_names=("B_Low", "B_Mid", "B_Upper", "B_Bandwidth", "B_Percent"))
+            newdf.ta.sma(close=df["Close"], length=50, append=True, col_names=("SMA50"))
+            newdf.ta.sma(close=df["Close"], length=200, append=True, col_names=("SMA200"))
+            newdf["52WeekHigh"] = df["Close"].rolling(window=200, min_periods=1).max()
+
+            payload = newdf.tail(1).to_dict('records')[0]
+            del payload['MACD_Histogram']
+            del payload['STOCH_D']
+            del payload['B_Bandwidth']
+            del payload['B_Percent']
+
+            payload["Ticker"] = ticker
+            payload["Open"] = df.iloc[-1]["Close"]
+            end = time.time()
+            payload["time_elapsed"] = end-start
+            return payload
+        except:
+            return 0
+
+api.add_resource(TickerInfo, "/tickerinfo")
 
 if __name__ == "__main__":
     app.run(debug=True)
